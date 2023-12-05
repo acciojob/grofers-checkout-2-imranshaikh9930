2b419d6bdffc4ae8a679e85aa1ad654cfbd3b181
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-12-05 14:34:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your code here
+function updateTotal() {
+    // Get all elements with data-ns-test="prices"
+    const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
+
+    // Calculate the total price
+    let totalPrice = 0;
+    priceElements.forEach(element => {
+        totalPrice += parseFloat(element.textContent);
+    });
+
+    // Create a new row for the total
+    const totalRow = document.createElement('tr');
+    const totalCell1 = document.createElement('td');
+    totalCell1.textContent = 'Total Price';
+    totalRow.appendChild(totalCell1);
+
+    const totalCell2 = document.createElement('td');
+    totalCell2.setAttribute('data-ns-test', 'grandTotal');
+    totalCell2.textContent = totalPrice.toFixed(2);
+    totalRow.appendChild(totalCell2);
+
+    // Add the total row to the table
+    const table = document.getElementById('groceryTable');
+    table.appendChild(totalRow);
+}
+
+// Call the updateTotal function
+updateTotal();
